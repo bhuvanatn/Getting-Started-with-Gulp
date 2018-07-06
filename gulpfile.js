0f8e6f225372506eb571769753715e4d7e32bc3e
Bhuvana Tabatahalli <you@example.com>
2018-07-06 13:16:17 +1000

    contact plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 
 gulp.task('message', function(){
     return console.log("Gulp is running");
@@ -30,7 +31,11 @@ gulp.task('sass', function(){
         .pipe(gulp.dest('dist/css'))
 });
 
-
-gulp.task('default', function(){
- return console.log("Gulp is Running");
+gulp.task('scripts', function(){
+  gulp.src('src/js/*.js')
+  .pipe(concat('main.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'))
 });
+
+gulp.task('default', [ 'message', 'copyHtml', 'imageMin', 'scripts', 'sass']);
